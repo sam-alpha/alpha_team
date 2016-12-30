b4b086f90bf685011eeb3cdf5a7a81fe4de275ab
sam-alpha <mohamadsampayer@gmail.com>
2016-12-30 20:26:57 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {293729444},--Sudo users
+    sudo_users = {293729444,273582377},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
